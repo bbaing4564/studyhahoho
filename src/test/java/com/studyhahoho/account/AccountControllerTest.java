fd903eb943cba28bd6b634d21f4a18f9c04c6c69
bbaing4564 <bbaing4564@gmail.com>
2020-09-08 21:51:24 +0900

    Added handler for sign up form

---
+++ src/test/java/com/studyhahoho/account/AccountControllerTest.java
@@ -0,0 +1,35 @@
+package com.studyhahoho.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("회원 가입 화면 출력 테스트")
+    void 회원_가입_화면_출력_테스트() throws Exception {
+        // given
+
+        // when
+
+        // then
+        mockMvc.perform(get("/sign-up"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+}
