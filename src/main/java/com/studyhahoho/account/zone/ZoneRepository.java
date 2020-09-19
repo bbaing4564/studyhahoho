84e9a48899214389c68c320d0f3f1eacc9e5db7f
bbaing4564 <bbaing4564@gmail.com>
2020-09-20 01:04:50 +0900

    Zone domain and init data

---
+++ src/main/java/com/studyhahoho/account/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.studyhahoho.account.zone;
+
+import com.studyhahoho.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
