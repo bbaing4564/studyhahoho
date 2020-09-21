6d73b9ddec00f7d2c07c8d0075b91ec9c0b10824
bbaing4564 <bbaing4564@gmail.com>
2020-09-21 14:22:15 +0900

    Refactoring import and modifying file name

--- src/main/java/com/studyhahoho/account/AccountController.java
+++ src/main/java/com/studyhahoho/account/AccountController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 
 @Controller
 @RequiredArgsConstructor
