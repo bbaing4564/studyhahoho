18e1895809d5c2feb7f1e6767a4661dd6c04b6f1
bbaing4564 <bbaing4564@gmail.com>
2020-09-28 22:41:38 +0900

    Add Study domain and Study

---
+++ src/main/java/com/studyhahoho/study/StudyService.java
@@ -0,0 +1,22 @@
+package com.studyhahoho.study;
+
+import com.studyhahoho.domain.Account;
+import com.studyhahoho.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyService {
+
+    private final StudyRepository studyRepository;
+
+    public Study createNewStudy(Study study, Account account) {
+        Study newStudy = studyRepository.save(study);
+        newStudy.addManager(account);
+        return newStudy;
+    }
+
+}
