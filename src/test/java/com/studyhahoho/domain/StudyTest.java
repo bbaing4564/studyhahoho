5690f9e56e7bd947e1232bb075d9af1f0921db31
bbaing4564 <bbaing4564@gmail.com>
2020-10-06 00:10:22 +0900

    Add tests for StudyControllers

---
+++ src/test/java/com/studyhahoho/domain/StudyTest.java
@@ -0,0 +1,104 @@
+package com.studyhahoho.domain;
+
+import com.studyhahoho.WithAccount;
+import com.studyhahoho.account.UserAccount;
+import com.studyhahoho.domain.Account;
+import com.studyhahoho.domain.Study;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+    
+    Study study;
+    Account account;
+    UserAccount userAccount;
+
+    @BeforeEach
+    void beforeEach() {
+        study = new Study();
+        account = new Account();
+        account.setNickname("hahoho");
+        account.setPassword("1234");
+        userAccount = new UserAccount(account);
+    }
+
+    @Test
+    @DisplayName("스터디 멤버인지 확인")
+    void 스터디_멤버인지_확인() throws Exception {
+        // given
+        study.addMember(account);
+        // when
+
+        // then
+        assertTrue(study.isMember(userAccount));
+    }
+
+    @Test
+    @DisplayName("스터디 관리자인지 확인")
+    void 스터디_관리자인지_확인() throws Exception {
+        // given
+        study.addManager(account);
+        // when
+
+        // then
+        assertTrue(study.isManager(userAccount));
+    }
+
+    @Test
+    @DisplayName("스터디가 비공개이거나 인원모집 중이 아니면 스터디 가입 불가능")
+    void 스터디가_비공개이거나_인원모집_중이_아니면_스터디_가입_불가능() throws Exception {
+
+        study.setPublished(true);
+        study.setRecruiting(false);
+        assertFalse(study.isJoinable(userAccount));
+
+        study.setPublished(false);
+        study.setRecruiting(true);
+        assertFalse(study.isJoinable(userAccount));
+    }
+
+    @Test
+    @DisplayName("스터디 공개 인원모집중이지만 스터디 멤버는 스터디 재가입 불가")
+    void 스터디_공개_인원모집중이지만_스터디_멤버는_스터디_재가입_불가() throws Exception {
+        // given
+
+        // when
+        study.setPublished(true);
+        study.setRecruiting(true);
+        study.addMember(account);
+
+        // then
+        assertFalse(study.isJoinable(userAccount));
+    }
+
+    @Test
+    @DisplayName("스터디 공개 인원모집중이지만 스터디 매니는 스터디 재가입 불가")
+    void 스터디_공개_인원모집중이지만_스터디_매니는_스터디_재가입_불가() throws Exception {
+        // given
+
+        // when
+        study.setPublished(true);
+        study.setRecruiting(true);
+        study.addManager(account);
+
+        // then
+        assertFalse(study.isJoinable(userAccount));
+    }
+
+    @Test
+    @DisplayName("스터디 공개 인원모집 멤버나 매니저가 아니라면 가입 가능")
+    void 스터디_공개_인원모집_멤버나_매니저가_아니라면_가입_가능() throws Exception {
+        // given
+
+        // when
+        study.setPublished(true);
+        study.setRecruiting(true);
+
+        // then
+        assertTrue(study.isJoinable(userAccount));
+    }
+
+}
--- src/test/java/com/studyhahoho/study/StudyControllerTest.java
+++ src/test/java/com/studyhahoho/study/StudyControllerTest.java
@@ -1,103 +1,169 @@
 package com.studyhahoho.study;
 
-import com.studyhahoho.account.UserAccount;
+import com.studyhahoho.WithAccount;
+import com.studyhahoho.account.AccountRepository;
 import com.studyhahoho.domain.Account;
 import com.studyhahoho.domain.Study;
-import org.junit.jupiter.api.BeforeEach;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
 class StudyControllerTest {
-    
-    Study study;
-    Account account;
-    UserAccount userAccount;
 
-    @BeforeEach
-    void beforeEach() {
-        study = new Study();
-        account = new Account();
-        account.setNickname("hahoho");
-        account.setPassword("1234");
-        userAccount = new UserAccount(account);
-    }
+    @Autowired protected MockMvc mockMvc;
+    @Autowired protected StudyService studyService;
+    @Autowired protected StudyRepository studyRepository;
+    @Autowired protected AccountRepository accountRepository;
 
+    @WithAccount("hahoho")
     @Test
-    @DisplayName("스터디 멤버인지 확인")
-    void 스터디_멤버인지_확인() throws Exception {
-        // given
-        study.addMember(account);
-        // when
-
-        // then
-        assertTrue(study.isMember(userAccount));
-    }
-
-    @Test
-    @DisplayName("스터디 관리자인지 확인")
-    void 스터디_관리자인지_확인() throws Exception {
-        // given
-        study.addManager(account);
-        // when
-
-        // then
-        assertTrue(study.isManager(userAccount));
-    }
-
-    @Test
-    @DisplayName("스터디가 비공개이거나 인원모집 중이 아니면 스터디 가입 불가능")
-    void 스터디가_비공개이거나_인원모집_중이_아니면_스터디_가입_불가능() throws Exception {
-
-        study.setPublished(true);
-        study.setRecruiting(false);
-        assertFalse(study.isJoinable(userAccount));
-
-        study.setPublished(false);
-        study.setRecruiting(true);
-        assertFalse(study.isJoinable(userAccount));
-    }
-
-    @Test
-    @DisplayName("스터디 공개 인원모집중이지만 스터디 멤버는 스터디 재가입 불가")
-    void 스터디_공개_인원모집중이지만_스터디_멤버는_스터디_재가입_불가() throws Exception {
+    @DisplayName("스터디 개설 폼 조회")
+    void 스터디_개설_폼_조회() throws Exception {
         // given
 
         // when
-        study.setPublished(true);
-        study.setRecruiting(true);
-        study.addMember(account);
-
+        mockMvc.perform(get("/new-study"))
         // then
-        assertFalse(study.isJoinable(userAccount));
+            .andExpect(status().isOk())
+            .andExpect(view().name("study/form"))
+            .andExpect(model().attributeExists("account"))
+            .andExpect(model().attributeExists("studyForm"));
     }
 
+    @WithAccount("hahoho")
     @Test
-    @DisplayName("스터디 공개 인원모집중이지만 스터디 매니는 스터디 재가입 불가")
-    void 스터디_공개_인원모집중이지만_스터디_매니는_스터디_재가입_불가() throws Exception {
+    @DisplayName("스터디 개설 완료")
+    void 스터디_개설_완료() throws Exception {
+        // given
+        String testPath = "test-path";
+        
+        // when
+        mockMvc.perform(post("/new-study")
+            .param("path", testPath)
+            .param("title", "study title")
+            .param("shortDescription", "short description of a study")
+            .param("fullDescription", "full description of a study")
+            .with(csrf()))
+        // then
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/study/" + testPath));
+
+        Study study = studyRepository.findByPath(testPath);
+        assertNotNull(study);
+        Account account = accountRepository.findByNickname("hahoho");
+        assertTrue(study.getManagers().contains(account));
+    }
+
+    @WithAccount("hahoho")
+    @Test
+    @DisplayName("스터디 개설 실패")
+    void 스터디_개설_실패() throws Exception {
         // given
 
         // when
-        study.setPublished(true);
-        study.setRecruiting(true);
-        study.addManager(account);
+        mockMvc.perform(post("/new-study")
+                .param("path", "wrong path")
+                .param("title", "study title")
+                .param("shortDescription", "short description of a study")
+                .param("fullDescription", "full description of a study")
+                .with(csrf()))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(view().name("study/form"))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("studyForm"))
+                .andExpect(model().attributeExists("account"));
 
-        // then
-        assertFalse(study.isJoinable(userAccount));
+        Study study = studyRepository.findByPath("test-path");
+        assertNull(study);
     }
 
+    @WithAccount("hahoho")
     @Test
-    @DisplayName("스터디 공개 인원모집 멤버나 매니저가 아니라면 가입 가능")
-    void 스터디_공개_인원모집_멤버나_매니저가_아니라면_가입_가능() throws Exception {
+    @DisplayName("스터디 조회")
+    void 스터디_조회() throws Exception {
         // given
+        Study study = new Study();
+        study.setPath("test-path");
+        study.setTitle("test study");
+        study.setShortDescription("short description");
+        study.setFullDescription("<p>full description</p>");
 
         // when
-        study.setPublished(true);
-        study.setRecruiting(true);
+        Account hahoho = accountRepository.findByNickname("hahoho");
+        studyService.createNewStudy(study, hahoho);
 
         // then
-        assertTrue(study.isJoinable(userAccount));
+        mockMvc.perform(get("/study/test-path"))
+                .andExpect(view().name("study/view"))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("study"));
     }
-    
+
+    @WithAccount("hahoho")
+    @Test
+    @DisplayName("스터디 가입")
+    void 스터디_가입() throws Exception {
+        // given
+        Account hahaha = createAccount("hahaha");
+        Study study = createStudy("test-study", hahaha);
+        // when
+
+        // then
+        mockMvc.perform(get("/study/" + study.getPath() + "/join"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + study.getPath() + "/members"));
+
+        Account hahoho = accountRepository.findByNickname("hahoho");
+        assertTrue(study.getMembers().contains(hahoho));
+    }
+
+    @WithAccount("hahoho")
+    @Test
+    @DisplayName("스터디 탈퇴")
+    void 스터디_탈퇴() throws Exception {
+        // given
+        Account hahaha = createAccount("hahaha");
+        Study study = createStudy("test-study", hahaha);
+        // when
+        Account hahoho = accountRepository.findByNickname("hahoho");
+        studyService.addMember(study, hahoho);
+        // then
+        mockMvc.perform(get("/study/" + study.getPath() + "/leave"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + study.getPath() + "/members"));
+
+        assertFalse(study.getMembers().contains(hahaha));
+    }
+
+    protected Study createStudy(String path, Account manager) {
+        Study study = new Study();
+        study.setPath(path);
+        studyService.createNewStudy(study, manager);
+        return study;
+    }
+
+    protected Account createAccount(String nickname) {
+        Account hahaha = new Account();
+        hahaha.setNickname(nickname);
+        hahaha.setEmail(nickname + "@email.com");
+        accountRepository.save(hahaha);
+        return hahaha;
+    }
+
 }
