7679a10daed1108f0a255778a0d51aaf224d4d3a
bbaing4564 <bbaing4564@gmail.com>
2020-10-10 16:58:04 +0900

    Add Event list view

--- src/main/java/com/studyhahoho/domain/Event.java
+++ src/main/java/com/studyhahoho/domain/Event.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NamedEntityGraph(
+        name = "Event.withEnrollments",
+        attributeNodes = @NamedAttributeNode("enrollments")
+)
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 public class Event {
@@ -80,6 +84,10 @@ public class Event {
             }
         }
         return false;
+    }
+
+    public int numberOfRemainSpots() {
+        return this.limitOfEnrollments - (int) this.enrollments.stream().filter(Enrollment::isAccepted).count();
     }
 
 }
