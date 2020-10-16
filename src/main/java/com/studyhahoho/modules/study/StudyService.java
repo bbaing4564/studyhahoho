7fd14fbe356cd0ca5c4ddebdce40047aa971249c
bbaing4564 <bbaing4564@gmail.com>
2020-10-16 17:28:08 +0900

    Add Notification infra

--- src/main/java/com/studyhahoho/modules/study/StudyService.java
+++ src/main/java/com/studyhahoho/modules/study/StudyService.java
@@ -1,11 +1,13 @@
 package com.studyhahoho.modules.study;
 
 import com.studyhahoho.modules.account.Account;
+import com.studyhahoho.modules.study.event.StudyCreatedEvent;
 import com.studyhahoho.modules.tag.Tag;
 import com.studyhahoho.modules.zone.Zone;
 import com.studyhahoho.modules.study.form.StudyDescriptionForm;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,10 +22,12 @@ public class StudyService {
 
     private final StudyRepository studyRepository;
     private final ModelMapper modelMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = studyRepository.save(study);
         newStudy.addManager(account);
+        eventPublisher.publishEvent(new StudyCreatedEvent(newStudy));
         return newStudy;
     }
 
