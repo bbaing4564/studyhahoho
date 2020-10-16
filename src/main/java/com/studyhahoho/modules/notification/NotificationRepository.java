a76349222926b81429b7568cb15759f2ba5059f1
bbaing4564 <bbaing4564@gmail.com>
2020-10-16 19:29:32 +0900

    Add Notification icon

--- src/main/java/com/studyhahoho/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyhahoho/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.studyhahoho.modules.notification;
 
+import com.studyhahoho.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
