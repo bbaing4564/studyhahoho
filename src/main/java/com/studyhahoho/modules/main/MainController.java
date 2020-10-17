218596425f37520e1114f96744da5cdd15232cd6
bbaing4564 <bbaing4564@gmail.com>
2020-10-17 22:32:00 +0900

    Add search study by keyword

--- src/main/java/com/studyhahoho/modules/main/MainController.java
+++ src/main/java/com/studyhahoho/modules/main/MainController.java
@@ -1,13 +1,21 @@
 package com.studyhahoho.modules.main;
 
-import com.studyhahoho.modules.account.CurrentAccount;
 import com.studyhahoho.modules.account.Account;
+import com.studyhahoho.modules.account.CurrentAccount;
+import com.studyhahoho.modules.study.Study;
+import com.studyhahoho.modules.study.StudyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final StudyRepository studyRepository;
 
     @GetMapping("/")
     public String home(@CurrentAccount Account account, Model model) {
@@ -20,5 +28,13 @@ public class MainController {
     @GetMapping("/login")
     public String login( ){
         return "/login";
+    }
+
+    @GetMapping("/search/study")
+    public String searchStudy(String keyword, Model model) {
+        List<Study> studyList = studyRepository.findByKeyword(keyword);
+        model.addAttribute(studyList);
+        model.addAttribute("keyword", keyword);
+        return "search";
     }
 }
